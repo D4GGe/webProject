623698d82b70e230d6b41e2fe85d546041c73cf0
Richard <richard.wecke@gmail.com>
2014-10-15 14:48:56 +0200

    Updating dem beans!

--- 7chan/src/main/java/group7/ctrl/PostBB.java
+++ 7chan/src/main/java/group7/ctrl/PostBB.java
@@ -1,21 +1,21 @@
 package group7.ctrl;
 
 import java.io.Serializable;
-import java.util.Collection;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct; 
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
  * @author Richard
  */
+@Named
+@RequestScoped
 public class PostBB implements Serializable{
+    
+    
     
 }
