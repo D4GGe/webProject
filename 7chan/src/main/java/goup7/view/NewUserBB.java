bb244a1b0bc5195389f6d24f7f6de146502623e7
Sanna Älegård <sanna.alegard@hotmail.com>
2014-10-20 20:31:07 +0200

    test

--- 7chan/src/main/java/goup7/view/NewUserBB.java
+++ 7chan/src/main/java/goup7/view/NewUserBB.java
@@ -21,14 +21,6 @@ import javax.inject.Named;
 public class NewUserBB implements Serializable{
     private String name;
     private String password;
-<<<<<<< HEAD
-    
-    @PostConstruct
-    public void post() {
-        
-    }
-=======
->>>>>>> 3a38016a7385744979e731e2741c2273597b911b
 
     /**
      * @return the name
--- 7chan/src/main/java/group7/ctrl/NewUserCtrl.java
+++ 7chan/src/main/java/group7/ctrl/NewUserCtrl.java
@@ -8,15 +8,8 @@ package group7.ctrl;
 
 import goup7.view.NewUserBB;
 import group7.core.IUserContainer;
-import group7.core.UserContainer;
 import java.io.Serializable;
-<<<<<<< HEAD
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.annotation.ManagedBean;
-=======
 import javax.ejb.EJB;
->>>>>>> 3a38016a7385744979e731e2741c2273597b911b
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -29,44 +22,23 @@ import javax.inject.Named;
 @RequestScoped
 
 public class NewUserCtrl implements Serializable{
-<<<<<<< HEAD
-    private SingletonForum sf;
-    private NewUserBB nubb;
-    
-    protected NewUserCtrl(){
-        
-    }
-    
-  @Inject
-    public NewUserCtrl(SingletonForum sf){
-        this.sf =sf;
-        
-    }
-=======
-   // private SingletonForum sf;
+  
     private NewUserBB newUserBB;
     @EJB
     private IUserContainer userContainer;
-    public NewUserCtrl(){
-        
-    }
->>>>>>> 3a38016a7385744979e731e2741c2273597b911b
+    
     @Inject
     public void setnubb(NewUserBB nubb){
         this.newUserBB = nubb;
     }
     
     public void save(){
-<<<<<<< HEAD
-     sf.getForum().getUserContainer().create(new User(nubb.getName(),nubb.getPassword()));
-               // Logger.getAnonymousLogger.log(Level.INFO, "dihuahduha");
-=======
+
         System.out.println("********************" + userContainer.test());
         System.out.println(newUserBB.getName()+"********************         " + newUserBB.getPassword());
         
       // sf.getForum().getUserContainer().create(new User(nubb.getName(),nubb.getPassword()));
-        
->>>>>>> 3a38016a7385744979e731e2741c2273597b911b
+
         
     }
 }
