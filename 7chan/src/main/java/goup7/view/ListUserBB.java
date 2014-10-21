8467eccee7d4da965aaff295e98bee6ebec692a7
Richard <richard.wecke@gmail.com>
2014-10-21 12:25:17 +0200

    I have made ListUserBB bean and a UserList xhtml file. Cannot get the project to run on my computer though.

---
+++ 7chan/src/main/java/goup7/view/ListUserBB.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goup7.view;
+
+import group7.core.IUserContainer;
+import group7.core.User;
+import group7.core.UserContainer;
+import java.io.Serializable;
+import java.util.Collection;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Richard
+ */
+public class ListUserBB implements Serializable{
+    
+    private int currentPage;
+    private final int pageSize = 10;
+    private int count;
+    @EJB
+    private IUserContainer userContainer;
+    protected ListUserBB(){
+        currentPage = 0;
+    
+}
+    @PostConstruct
+    public void user(){
+        count = userContainer.count();
+    }
+    
+    //Listing all the users at once! Not sure if implementing code for different pages is necessary...
+    public Collection<User> getList(){
+        return userContainer.findAll();
+    }
+    
+    public void next() {
+        if ((currentPage + 1) * pageSize < count) {
+            currentPage = currentPage + 1;
+        }
+    }
+
+    public void prev() {
+        if (currentPage > 0) {
+            currentPage = currentPage - 1;
+        }
+    }
+    
+    // ---- Getters/Setters -------------
+   
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int count() {
+        return count;
+    }
+    
+}
