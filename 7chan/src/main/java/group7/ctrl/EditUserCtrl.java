1b41bf436809824e43e56ddd87e5fa8668add565
D4GGe <D4GGe92@gmail.com>
2014-10-26 20:19:35 +0100

    changes in design and EditUserCtrl

--- 7chan/src/main/java/group7/ctrl/EditUserCtrl.java
+++ 7chan/src/main/java/group7/ctrl/EditUserCtrl.java
@@ -7,6 +7,7 @@ package group7.ctrl;
 
 import goup7.view.EditUserBB;
 import group7.core.IUserContainer;
+import group7.util.Md5;
 import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
@@ -34,7 +35,10 @@ public class EditUserCtrl implements Serializable{
     }
     
     public void edit(){
-        loginBean.getUser().setPassword(editUserBB.getPassword());
+        if(!loginBean.getUser().getPassword().equals(editUserBB.getPassword()) && editUserBB.getPassword().length()> 1){
+        loginBean.getUser().setPassword(Md5.hash(editUserBB.getPassword()));    
+        }
+        
         loginBean.getUser().setFirstname(editUserBB.getFirstname());
         loginBean.getUser().setLastname(editUserBB.getLastname());
         loginBean.getUser().setAge(editUserBB.getAge());
